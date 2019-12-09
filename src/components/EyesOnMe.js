2fa8d61fc4c2aded3b21cced9342a41a4199ab5c
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-09 06:50:04 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+import  React, {Component} from 'react';
+
+export default class Keypad extends Component{
+    handleKeyUp = () =>{
+        console.log('Entering password...');
+    }
+    render(){
+        return(
+            <input type="password" onKeyUp={this.handleKeyUp} />
+        );
+    }
+}
